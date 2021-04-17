77dc3085c2584497481ab50c033dc36a6277b13a
3dlifeStudio <mjfernandez3d@gmail.com>
2021-04-17 21:27:26 +0100

    Initial Commit

---
+++ AlgoBubbleGame/AlgoBubbleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoBubbleGameApp.swift
+//  AlgoBubbleGame
+//
+//  Created by Mario Fernandes on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlgoBubbleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgoBubbleGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlgoBubbleGame
+//
+//  Created by Mario Fernandes on 17/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
